32fb3717346c5db8043e160dde1abb82e5e42ebe
Bakanours <incruste_2000@hotmail.com>
2019-06-06 20:57:40 +0200

    tt

---
+++ nav.php
@@ -0,0 +1,29 @@
+<?php 
+?>
+<!DOCTYPE HTML>
+<html lang="fr">
+
+<nav>
+
+    <ul>
+        <li><a href="index.html">accueil</a></li>
+    </ul>
+    <ul>
+        <li><a href="meubles.html">meubles</a></li>
+    </ul>
+    <ul>
+        <li><a href="luminaires.html">luminaires</a></li>
+    </ul>
+    <ul>
+        <li><a>accessoires</a></li>
+    </ul>
+    <ul>
+        <li><a>contact</a></li>
+    </ul>
+
+</nav>
+
+</html>
+
+<?php
+?>
